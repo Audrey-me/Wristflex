18f4c4c1d2173a4444e9d206d418bb4d2e8a2e65
tezon <tezonteam@gmail.com>
2023-02-21 03:15:13 -0800

    API for login and signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,143 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password} = req.body
+
+  if (!name || !email || !password) {
+    res.status(400).json({ message : 'Please add all fields'})
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400).json({ message : 'User already exists'})
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message : 'Invalid User Data'})
+  }
+})
+
+// @desc    Authenticate a user
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  // Check for user email
+  const user = await User.findOne({ email })
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message : 'Invalid Credentials'})
+  }
+})
+
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  const {_id, name, email} = await User.findById(req.user.id)
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  })
+})
+
+// get oldUser id
+const oldUser = asyncHandler(async (req, res) => {
+  const {email} = req.body
+  const user = await User.findOne({email})
+  if (user) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else if(!user){
+    res.status(400).json({message: 'Email does not exist'})
+  }
+  else{
+    res.status(500).json({message: 'Network error'})
+  }
+})
+
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.pic = req.body.pic || user.pic;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      pic: updatedUser.pic,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    res.status(400).json({ message : 'User not Found'})
+  }
+});
+
+
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+  oldUser,
+  updateUserProfile,
+};
---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+    },
+    fileName: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+module.exports = mongoose.model("user", userSchema);
