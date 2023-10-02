2f7f9d128a0cd17e18cfc75a67f5570f2be516bf
kosisochukwu <kosisochukwua@phreetech.com>
2023-10-02 11:10:46 +0100

    FAQ page

--- frontend/src/components/views/FAQs.js
+++ frontend/src/components/views/FAQs.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import Navbar from '../partials/Navbar';
 import Footer from "../partials/Footer"
-import MyAccordion from '../partials/MyAccordion';
+import { Accordion, Card } from "react-bootstrap";
 
-const FAQs = () => {
+
+export default function Profile() {
   return (
     <>
     <Navbar/>
         <section className='faq-container'>
             <h3>FREQUENTLY ASKED QUESTIONS</h3>
-             <MyAccordion/>
+            <Accordion>
+                        <Card>
+                            <Accordion.Toggle as={Card.Header} eventKey="0">
+                                TAB 1
+                            </Accordion.Toggle>
+
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>This is first tab body</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+              </Accordion>          
         </section>
         <Footer/>
         </>
   )
 };
 
-export default FAQs;
+
 
 
   
